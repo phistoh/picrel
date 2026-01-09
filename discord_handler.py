0ceba4888afe4124afd6ae12cc84b7eb5ce1bced
phistoh <phis@mogobb.de>
2026-01-09 09:51:06 +0100

    embed method

--- discord_handler.py
+++ discord_handler.py
@@ -14,6 +14,16 @@ def send_message(msg: str):
     response = webhook.execute()
 
 
+def send_embed(title: str, msg: str, filename: str):
+    webhook = DiscordWebhook(url=config["Discord"]["WebhookUrl"])
+    embed = DiscordEmbed(title=title, description=msg, color="03b2f8")
+    with open(filename, "rb") as f:
+        webhook.add_file(file=f.read(), filename=filename)
+    embed.set_image(url=f"attachment://{filename}")
+    webhook.add_embed(embed)
+    response = webhook.execute()
+
+
 def send_image(filename: str):
     webhook = DiscordWebhook(url=config["Discord"]["WebhookUrl"])
 
